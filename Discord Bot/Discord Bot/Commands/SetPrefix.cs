fdcdcf12f43b31b53e4a52b718d3da4f93a94d6b
jacobguin <technogeekntraining@gmail.com>
2019-10-11 19:09:06 -0400

    aded a Setprefix command

---
+++ Discord Bot/Discord Bot/Commands/SetPrefix.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Commands
+{
+    public class SetPrefix : ModuleBase<SocketCommandContext>
+    {
+        [Command("setprefix"), Summary("Changes the user prefix")]
+        public async Task sp(params string[] prefix)
+        {
+            if (prefix.Length > 1)
+            {
+                await Context.Channel.SendMessageAsync("That is not a valid prefix!");
+            }
+            else if (prefix.Length < 1)
+            {
+                await Context.Channel.SendMessageAsync("Please provide a prefix!");
+            }
+            else
+            {
+                if (prefix[0].Length != 1)
+                {
+                    await Context.Channel.SendMessageAsync("That is not a valid prefix!");
+                }
+                else
+                {
+                    Database.Update("Users", "Prefix", "ID", $"{Context.User.Id}{Context.Guild.Id}", prefix[0]);
+                    await Context.Channel.SendMessageAsync($"your prefix is now '{prefix[0]}'");
+                }
+            }
+        }
+    }
+}
