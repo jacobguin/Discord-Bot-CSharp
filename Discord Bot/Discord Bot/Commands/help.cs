7e6db20e58ad900056a5aeb53ff25102b118b3a6
jacobguin <technogeekntraining@gmail.com>
2019-10-11 18:27:02 -0400

    Added a help command

---
+++ Discord Bot/Discord Bot/Commands/help.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Commands
+{
+    public class Help : ModuleBase<SocketCommandContext>
+    {
+        [Command("Help"), Summary("Help Command")]
+        public async Task help(params string[] Command)
+        {
+            string Prefix = Utils.GetPrefix(Context);
+            if (Command.Length == 0)
+            {
+
+                EmbedBuilder Embed = new EmbedBuilder();
+                Embed.Title = "Jacob Bot Commands";
+
+                string DEC = "```markdown\n";
+
+                foreach (CommandInfo Info in Program.Commands.Commands)
+                {
+                    DEC = DEC + "- " + Prefix + Info.Name + "\n";
+                }
+
+                Embed.WithDescription($"{DEC}```");
+                Embed.WithFooter("These are all the commands for the bot");
+
+                await Context.Channel.SendMessageAsync("", false, Embed.Build());
+            }
+            else if (Command.Length == 1)
+            {
+                foreach (CommandInfo Info in Program.Commands.Commands)
+                {
+                    if (Command[0].ToLower() == Info.Name.ToLower())
+                    {
+                        await Context.Channel.SendMessageAsync(Info.Summary);
+                    }
+                }
+            }
+            else
+            {
+                //IDK what to do here
+            }
+        }
+    }
+}
