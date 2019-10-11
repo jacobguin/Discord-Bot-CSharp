de4b5464dbe1c2777fe62861a6e63c5fd6b7d0bc
jacobguin <technogeekntraining@gmail.com>
2019-10-11 19:57:11 -0400

    Working on audio system

---
+++ Discord Bot/Discord Bot/Code Support/Music/Backend.cs
@@ -0,0 +1,40 @@
+﻿using Discord.Audio;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Code_Support.Music
+{
+    public static class Backend
+    {
+        private static AudioOutStream discord;
+        private static Stream output;
+
+        private static Process YTStreame(string url)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C youtube-dl.exe --no-check-certificate -f bestaudio -o - {url} | ffmpeg.exe -i pipe:0 -f s16le -ar 48000 -ac 2 pipe:1",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+            });
+        }
+
+        public static async Task SendUrlAsync(IAudioClient client, string url)
+        {
+            using (Process ffmpeg = YTStreame(url))
+            {
+                output = ffmpeg.StandardOutput.BaseStream;
+                discord = client.CreatePCMStream(AudioApplication.Mixed);
+                try { await output.CopyToAsync(discord); }
+                finally { await discord.FlushAsync(); }
+            }
+        }
+    }
+}
---
+++ Discord Bot/Discord Bot/Code Support/Music/Queue.cs
@@ -0,0 +1,70 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Code_Support.Music
+{
+    public static class Queue
+    {
+        public enum Type
+        {
+            Playlist,
+            Youtube
+        }
+
+        public static class Playlist
+        {
+            public static string[] Songs()
+            {
+                return new string[] { "Not Setup yet" };
+            }
+        }
+
+        public static string[] List(SocketCommandContext Context, out Type[] Out)
+        {
+            string RAW = Database.Read("Music", "Server_ID", Context.Guild.Id.ToString(), "Queue");
+            if (string.IsNullOrEmpty(RAW))
+            {
+                Out = null;
+                return null;
+            }
+            else
+            {
+                RAW = RAW.Replace("|yt|", "|yt").Replace("|playlist|", "|playlist");
+                string[] raw = RAW.Split('|');
+                string[] output = null;
+                Type[] Output = null;
+                int current = 0;
+                foreach (string Result in raw)
+                {
+                    if (Result.StartsWith("yt"))
+                    {
+                        output[current] = Result.Replace("yt", "");
+                        Output[current] = Type.Youtube;
+                        current++;
+                    }
+                    else if (Result.StartsWith("playlist"))
+                    {
+                        //Will not ocure Yet, this may change in the fucture.
+                        output[current] = Result.Replace("playlist", "");
+                        Output[current] = Type.Playlist;
+                        current++;
+                    }
+                }
+                Out = Output;
+                return output;
+            }
+        }
+
+        public static string FirstInQueue(SocketCommandContext Context, out Type Out)
+        {
+            Type[] temp = null;
+            string Output = List(Context, out temp)[0];
+            Out = temp[0];
+            return Output;
+        }
+    }
+}
