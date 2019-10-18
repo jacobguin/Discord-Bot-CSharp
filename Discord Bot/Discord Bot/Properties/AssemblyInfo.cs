14b9f801ebba506e430875176e08a47a4cc878d5
jacobguin <technogeekntraining@gmail.com>
2019-10-18 16:56:51 -0400

    Updated assembly info

--- Discord Bot/Discord Bot/Properties/AssemblyInfo.cs
+++ Discord Bot/Discord Bot/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Discord Bot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A discord bot for everyone made in Discord.Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jacob Guin")]
 [assembly: AssemblyProduct("Discord Bot")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
